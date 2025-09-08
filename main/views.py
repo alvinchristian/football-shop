02091539cac6291e14b0e91b9fcff31e5b0f9d0f
alvinchristian <alvch6@gmail.com>
2025-09-08 20:47:55 +0700

    deploy to pws

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_template(request):
     context = {
+        'shop': 'Full Time Gear',
         'name': 'Alvin Christian Halim',
         'class': 'PBP F'
     }
